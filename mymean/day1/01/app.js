299a18c2e9dd1298b349206b943fd6946d613015
Dnyaneshwar <dnyaneshwarshinde003@gmail.com>
2019-11-15 16:41:22 +0530

    angular first update

---
+++ mymean/day1/01/app.js
@@ -0,0 +1,10 @@
+var someObject = require("./x");
+ 
+ someObject.SayHi("Dnyaneshwar");
+ console.log(someObject.someData);
+// console.log("hello");
+ console.log(someObject);
+
+ console.log(module);
+
+someObject.SayHi("Dnyaneshwar");
